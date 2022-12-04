b5837e2d6dcc304971e97a5d3b696893237925f9
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-12-04 19:03:05 +0530

    attempt fix on pad texture loading

--- src/MarbleWorld.hx
+++ src/MarbleWorld.hx
@@ -300,7 +300,18 @@ class MarbleWorld extends Scheduler {
 			"sound/spawn.wav",
 			"sound/ready.wav",
 			"sound/set.wav",
-			"sound/go.wav"
+			"sound/go.wav",
+			"sound/alarm.wav",
+			"sound/alarm_timeout.wav",
+			"sound/missinggems.wav",
+			"shapes/images/glow_bounce.dts",
+			"shapes/images/glow_bounce.png",
+			"shapes/images/helicopter.dts",
+			"shapes/images/helicopter.jpg",
+			"shapes/pads/white.jpg", // These irk us a lot because ifl shit
+			"shapes/pads/red.jpg",
+			"shapes/pads/blue.jpg",
+			"shapes/pads/green.jpg",
 		];
 		for (file in marblefiles) {
 			worker.loadFile(file);
