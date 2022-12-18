05c62eee573d9e6a205ab253278d2cf7affa5ba8
Kayden Tebau <me@nullob.si>
2022-12-18 00:07:25 -0800

    Update circleCI config

--- package-macos.sh
+++ package-macos.sh
@@ -16,6 +16,9 @@ sed -i "" -e "s/@VERSION@/$1/" "macos-dist/MarbleBlast Platinum.app/Contents/Inf
 
 # Copy binary
 cp native/marblegame "macos-dist/MarbleBlast Platinum.app/Contents/MacOS/marblegame" || exit $?
+# Copy data
+cp -r data "macos-dist/MarbleBlast Platinum.app/Contents/Resources/" || exit $?
+
 cd "macos-dist/MarbleBlast Platinum.app/Contents/MacOS/"
 install_name_tool -change ui.hdll @rpath/ui.hdll \
 	-change openal.hdll @rpath/openal.hdll \
