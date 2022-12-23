e6fb205e999cd93b9a47380e5ff70760e0a985f9
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-12-23 22:11:08 +0530

    attempt fix clipping and fix onscreen text

--- src/gui/GuiTextListCtrl.hx
+++ src/gui/GuiTextListCtrl.hx
@@ -103,7 +103,7 @@ class GuiTextListCtrl extends GuiControl {
 			this.flow.maxHeight = cast htr.extent.y;
 			this.flow.multiline = true;
 			this.flow.layout = Stack;
-			this.flow.overflow = FlowOverflow.Limit;
+			this.flow.overflow = FlowOverflow.Hidden;
 			if (scene2d.contains(this.flow))
 				scene2d.removeChild(this.flow);
 
