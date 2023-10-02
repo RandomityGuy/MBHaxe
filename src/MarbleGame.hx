8024bbf2c4c1515e62e366123f8c323d084d8de1
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2023-10-02 12:21:17 +0530

    fix bugs

--- src/MarbleGame.hx
+++ src/MarbleGame.hx
@@ -224,6 +224,13 @@ class MarbleGame {
 
 	public function quitMission() {
 		world.setCursorLock(false);
+		if (!Settings.levelStatistics.exists(world.mission.path)) {
+			Settings.levelStatistics.set(world.mission.path, {
+				oobs: 0,
+				respawns: 0,
+				totalTime: 0,
+			});
+		}
 		var stats = Settings.levelStatistics[world.mission.path];
 		Analytics.trackLevelQuit(world.mission.title, world.mission.path, Std.int(world.timeState.timeSinceLoad * 1000), stats.oobs, stats.respawns,
 			Settings.optionsSettings.rewindEnabled);
