bb0e3535f23680d4f0bcd7a8d5808c55e83ae8c1
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2023-12-24 18:31:54 +0530

    fix 2

--- src/Settings.hx
+++ src/Settings.hx
@@ -165,7 +165,7 @@ class Settings {
 	public static function applySettings() {
 		#if hl
 		Window.getInstance().resize(optionsSettings.screenWidth, optionsSettings.screenHeight);
-		Window.getInstance().displayMode = optionsSettings.isFullScreen ? Fullscreen : Windowed;
+		Window.getInstance().displayMode = optionsSettings.isFullScreen ? Borderless : Windowed;
 		#end
 		AudioManager.updateVolumes();
 
@@ -305,7 +305,7 @@ class Settings {
 		load();
 		#if hl
 		Window.getInstance().resize(optionsSettings.screenWidth, optionsSettings.screenHeight);
-		Window.getInstance().displayMode = optionsSettings.isFullScreen ? FullscreenResize : Windowed;
+		Window.getInstance().displayMode = optionsSettings.isFullScreen ? Borderless : Windowed;
 		uiScale = 1 / Window.getInstance().windowToPixelRatio;
 		#end
 		#if js
