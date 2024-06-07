f5be65234fca3330171799035b73bfc9feae06d8
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-06-07 20:58:25 +0530

    fix caret color

--- src/gui/GuiTextInput.hx
+++ src/gui/GuiTextInput.hx
@@ -82,4 +82,9 @@ class GuiTextInput extends GuiControl {
 		}
 		#end
 	}
+
+	public function setCaretColor(col:Int) {
+		text.cursorTile = h2d.Tile.fromColor(col, Std.int(1 / hxd.Window.getInstance().windowToPixelRatio), text.font.size);
+		text.cursorTile.dy = 2 / hxd.Window.getInstance().windowToPixelRatio;
+	}
 }
