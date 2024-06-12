8f30e87d9d86a9a4a838bd8d07e8705001b6230e
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-06-12 19:38:33 +0530

    fix joinservergui

--- src/gui/JoinServerGui.hx
+++ src/gui/JoinServerGui.hx
@@ -24,12 +24,7 @@ class JoinServerGui extends GuiImage {
 		@:privateAccess arial14b.loader = ResourceLoader.loader;
 		var arial14 = arial14b.toSdfFont(cast 21 * Settings.uiScale, h2d.Font.SDFChannel.MultiChannel);
 
-		#if hl
-		var scene2d = hxd.Window.getInstance();
-		#end
-		#if js
-		var scene2d = MarbleGame.instance.scene2d;
-		#end
+		var scene2d = MarbleGame.canvas.scene2d;
 
 		var offsetX = (scene2d.width - 1280) / 2;
 		var offsetY = (scene2d.height - 720) / 2;
