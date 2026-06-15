fdfb2b0452609bc3bedadf625f960929aa485c97
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2026-06-15 23:55:41 +0100

    dont allow resolution change on mobile

--- src/gui/OptionsDlg.hx
+++ src/gui/OptionsDlg.hx
@@ -401,22 +401,22 @@ Extensions: EAX 2.0, EAX 3.0, EAX Unified, and EAX-AC3";
 		audTxtWndo.addChild(audInfo);
 
 		applyFunc = () -> {
-			if (gfx640480.pressed) {
-				Settings.optionsSettings.screenWidth = 640;
-				Settings.optionsSettings.screenHeight = 480;
-			}
-			if (gfx800600.pressed) {
-				Settings.optionsSettings.screenWidth = 800;
-				Settings.optionsSettings.screenHeight = 600;
-			}
-			if (gfx1024768.pressed) {
-				Settings.optionsSettings.screenWidth = 1024;
-				Settings.optionsSettings.screenHeight = 768;
-			}
-			if (gfxFull.pressed)
-				Settings.optionsSettings.isFullScreen = true;
-			else
-				Settings.optionsSettings.isFullScreen = false;
+			// if (gfx640480.pressed) {
+			// 	Settings.optionsSettings.screenWidth = 640;
+			// 	Settings.optionsSettings.screenHeight = 480;
+			// }
+			// if (gfx800600.pressed) {
+			// 	Settings.optionsSettings.screenWidth = 800;
+			// 	Settings.optionsSettings.screenHeight = 600;
+			// }
+			// if (gfx1024768.pressed) {
+			// 	Settings.optionsSettings.screenWidth = 1024;
+			// 	Settings.optionsSettings.screenHeight = 768;
+			// }
+			// if (gfxFull.pressed)
+			// 	Settings.optionsSettings.isFullScreen = true;
+			// else
+			// 	Settings.optionsSettings.isFullScreen = false;
 			// if (gfx16.pressed)
 			// 	Settings.optionsSettings.colorDepth = 0;
 			// else
