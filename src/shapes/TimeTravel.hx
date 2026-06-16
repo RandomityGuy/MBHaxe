eaa6abdf46135868597b2cae8b8787162b1bfb78
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2026-06-16 23:12:31 +0100

    fix these names

--- src/shapes/TimeTravel.hx
+++ src/shapes/TimeTravel.hx
@@ -23,7 +23,7 @@ class TimeTravel extends PowerUp {
 			this.timeBonus = -MisParser.parseNumber(element.timepenalty) / 1000;
 		}
 
-		this.pickUpName = '${this.timeBonus} second Time ${this.timeBonus >= 0 ? 'Modifier' : 'Penalty'}';
+		this.pickUpName = '${this.timeBonus} second Time Travel';
 		this.cooldownDuration = 1e8;
 		this.useInstancing = true;
 		this.autoUse = true;
