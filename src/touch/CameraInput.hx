f515f741ad943ed6400fbe4d1ec237e6f38cbb71
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2026-08-16 12:07:49 +0100

    camera smoothing for touch

--- src/touch/CameraInput.hx
+++ src/touch/CameraInput.hx
@@ -20,6 +20,11 @@ class CameraInput {
 
 	var collider:GuiGraphics;
 
+	static inline var DELTA_BUFFER_SIZE = 3;
+
+	var deltaBufferX:Array<Float> = [];
+	var deltaBufferY:Array<Float> = [];
+
 	public function new() {
 		var width = MarbleGame.canvas.scene2d.width;
 		var height = MarbleGame.canvas.scene2d.height;
@@ -62,6 +67,8 @@ class CameraInput {
 			this.identifier = e.touchId;
 			prevMouse.x = e.relX;
 			prevMouse.y = e.relY;
+			deltaBufferX.resize(0);
+			deltaBufferY.resize(0);
 		}
 
 		interactive.onMove = (e) -> {
@@ -96,10 +103,27 @@ class CameraInput {
 						inpY = 0;
 				}
 
+				deltaBufferX.push(inpX);
+				deltaBufferY.push(inpY);
+				if (deltaBufferX.length > DELTA_BUFFER_SIZE)
+					deltaBufferX.shift();
+				if (deltaBufferY.length > DELTA_BUFFER_SIZE)
+					deltaBufferY.shift();
+
+				var smoothX = 0.0;
+				for (v in deltaBufferX)
+					smoothX += v;
+				smoothX /= deltaBufferX.length;
+
+				var smoothY = 0.0;
+				for (v in deltaBufferY)
+					smoothY += v;
+				smoothY /= deltaBufferY.length;
+
 				var dt = MarbleGame.instance.world.timeState.dt;
 
-				MarbleGame.instance.world.marble.camera.orbit(applyNonlinearScale((inpX / dt) * (1 / 60.0)) * (1 / 60.0) * 20,
-					applyNonlinearScale((inpY / dt) * (1 / 60.0)) * (1 / 60.0) * 20, true);
+				MarbleGame.instance.world.marble.camera.orbit(applyNonlinearScale((smoothX / dt) * (1 / 60.0)) * (1 / 60.0) * 35,
+					applyNonlinearScale((smoothY / dt) * (1 / 60.0)) * (1 / 60.0) * 35, true);
 
 				if (inpX != 0)
 					prevMouse.x = e.relX;
@@ -118,6 +142,8 @@ class CameraInput {
 
 			pressed = false;
 			this.identifier = -1;
+			deltaBufferX.resize(0);
+			deltaBufferY.resize(0);
 		}
 	}
 
